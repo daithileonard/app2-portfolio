ddc18a420d40d1f6946786ddc754cada7f49fa4d
daithileonard <DAITHILEONARD@GMAIL.COM>
2023-04-14 09:56:16 +0100

    Render the titles

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas
+from PIL import Image
 
 st.set_page_config(layout="wide")
 
@@ -13,6 +15,22 @@ with col2:
     something about me
     many lines"""
     st.info(content)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+col3, col4 = st.columns(2)
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image('images/1.png')
+        # image(Image.open('images/1.png'))
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
 
 footer = """
 This is a label but it doesnt fit into the columns created above, This is a label but it doesnt fit into the columns created above
